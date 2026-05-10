4c8c48f7ea244d3463e8ede95efbad59027d4fb6
dilaraj <dilara.ajaj369@gmail.com>
2026-05-11 00:01:25 +1000

    creating first half of component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import illustrationHero from './assets/images/illustration-hero.svg'
 
 function App() {
 
   return (
-    <div>
-      Hello World
+    <div className='order-summary-container'>
+      <div className="order-summary-img-container">
+        <img src={illustrationHero} alt="Illustration Hero Image" className='order-hero-img'/>
+      </div>
+      <div className="order-summary-text">
+        <div className="summary-header">Order Summary</div>
+        <div className="summary-subText">You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</div>
+        <div className="order-item">order-item</div>
+        <div className="payment-btn">Proceed to Payment</div>
+        <div className="cancel-btn">Cancel Order</div>
+      </div>
     </div>
   )
 }
