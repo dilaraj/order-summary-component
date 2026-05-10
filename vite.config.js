7dc19ac31974cbf6448004c65fef8f5842db03b1
dilaraj <dilara.ajaj369@gmail.com>
2026-05-10 23:04:49 +1000

    Configuring github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {host: true}
+  server: {host: true},
+  base: '/order-summary-component'
 })
